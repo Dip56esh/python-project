945b2487b5b47589c724a41b7cb1469c05fe8ac8
Dip56esh <dippeshd56@gmail.com>
2024-03-03 16:56:03 +0545

    first commit

---
+++ static/js/cart.js
@@ -0,0 +1,38 @@
+var upadteBtns=document.getElementsByClassName( "update-cart")
+
+for (var i = 0; i < upadteBtns.length; i++){
+    upadteBtns[i].addEventListener('click', function () {
+        var productId = this.dataset.product
+        var action = this.dataset.action
+        console.log('productId:', productId, 'action :', action)
+        
+        console.log('User:', user)
+        if (user == 'AnonymousUser') {
+            console.log('Not logged in..')
+        }
+        else {
+            updateUserOrder(productId,action)
+        }
+
+    })
+}
+
+
+function updateUserOrder(productId, action) {
+    console.log("User logged in sending data..")
+
+    var url = '/update_item/'
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+                            'X-CSRFToken':csrftoken    },
+        body:JSON.stringify({'productId': productId, 'action': action})
+    })
+        .then(response => response.json())
+    
+        .then((data) => {
+            console.log('data:', data)
+            location.reload()
+    })
+}
---
+++ store/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+	#Leave as empty string for base url
+	path('', views.store, name="store"),
+	path('cart/', views.cart, name="cart"),
+	path('checkout/', views.checkout, name="checkout"),
+    path('login/', views.login, name="login"),
+
+     path('process_login/', views.processLogin, name="process_login"),
+
+    
+	path('update_item/', views.updateItem, name="update_item"),
+    path('process_order/', views.processOrder, name="process_order"),
+
+    
+
+
+]
